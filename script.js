61ceacf98f895248bb78d08806593979f309212d
David O'Reilly <hello@dsoreilly.me>
2020-07-22 17:32:13 +0100

    Update content and styling

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let heading = document.getElementById('title')
-    heading.innerText = heading.innerText.replace(/\'/g, '/')
-})
+  const heading = document.getElementById('title');
+  const logo = document.getElementById('logo');
+  const paragraph = document.getElementById('paragraph');
+  heading.innerText = heading.innerText.replace(/\'/g, '/');
+  paragraph.style.display = 'none';
+  logo.addEventListener('mouseenter', () => paragraph.style.display = 'block' );
+  logo.addEventListener('mouseleave', () => paragraph.style.display = 'none');
+  heading.addEventListener('mouseenter', () => {
+    heading.innerText = heading.innerText.replace(/\//g, '\\');
+    paragraph.style.display = 'block';
+  });
+  heading.addEventListener('mouseleave', () => {
+    heading.innerText = heading.innerText.replace(/\\/g, '/');
+    paragraph.style.display = 'none';
+  });
+});
