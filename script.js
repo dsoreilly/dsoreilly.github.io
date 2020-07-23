cdffb54710b83f15702f2b6a1df40c61b2d2ae83
David O'Reilly <hello@dsoreilly.me>
2020-07-23 07:34:18 +0100

    Minify script and style sheet

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const heading = document.getElementById('title');
   const logo = document.getElementById('logo');
   const paragraph = document.getElementById('paragraph');
-  heading.innerText = heading.innerText.replace(/\'/g, '/');
+  
   paragraph.style.display = 'none';
+
   logo.addEventListener('mouseenter', () => paragraph.style.display = 'block' );
   logo.addEventListener('mouseleave', () => paragraph.style.display = 'none');
+
+  heading.innerText = heading.innerText.replace(/\'/g, '/');
   heading.addEventListener('mouseenter', () => {
     heading.innerText = heading.innerText.replace(/\//g, '\\');
     paragraph.style.display = 'block';
