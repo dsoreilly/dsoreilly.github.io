df7cc11d292b6d538c85aac9169327f2f7f38862
David O'Reilly <hello@dsoreilly.me>
2022-12-07 15:02:39 +0000

    whitespace formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,20 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    content: ['./src/index.html'],
-    theme: {
-        extend: {
-            fontFamily: {
-                'display': ['Hubot Sans', ...defaultTheme.fontFamily.sans],
-            },
-            keyframes: {
-                gradient: {
-                    '0%': { 'background-position': '0% 50% '},
-                    '50%': { 'background-position': '100% 50%' },
-                    '100%': { 'background-position': '50% 0' },
-                },
-            },
+  content: ['./src/index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'display': ['Hubot Sans', ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        gradient: {
+          '0%': { 'background-position': '0% 50% ' },
+          '50%': { 'background-position': '100% 50%' },
+          '100%': { 'background-position': '50% 0' },
         },
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }
