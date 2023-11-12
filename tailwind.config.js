690ffa27d780504ed99c32c4df83f4135cf0f308
David O'Reilly <hello@dsoreilly.me>
2023-11-12 15:27:52 +0000

    ESM config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-/** @type {import("tailwindcss").Config} */
-module.exports = {
-  content: ["./src/**/*.html"],
-  plugins: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     fontFamily: {
       sans: ["'IBM Plex Sans'", "sans-serif"],
