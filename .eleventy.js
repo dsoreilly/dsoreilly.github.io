585549c2586502e530f10ec4ff3dafe5b5dd60ae
David O'Reilly <git@dsoreilly.me>
2026-01-29 12:24:19 +0000

    chore: update ignore configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,11 @@
 export default function(eleventyConfig) {
-  // pass through CSS files
+  // Pass through CSS files.
   eleventyConfig.addPassthroughCopy("css");
 
-  // filter to exclude draft posts in build mode
+  // Filter to exclude draft posts in build mode.
   eleventyConfig.addPreprocessor("drafts", "*", (data) => {
 		if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
 			return false;
 		}
 	});
-
-  // ignore README.md files
-  eleventyConfig.ignores.add("README.md");
 };
