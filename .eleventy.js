644cc52b52583d9618bf6033164055b188f2f9b2
David O'Reilly <git@dsoreilly.me>
2026-04-29 12:46:12 +0100

    Astro migration

--- .eleventy.js
+++
@@ -1,6 +0,0 @@
-export default function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("fonts");
-
-  eleventyConfig.ignores.add("README.md");
-}
